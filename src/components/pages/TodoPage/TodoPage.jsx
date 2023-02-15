8f87c0925695be9a565278a5607b8c80987c7802
Gr4childo <samohkin.2012@gmail.com>
2023-02-15 16:23:18 +0300

    add title and checked

--- src/components/pages/TodoPage/TodoPage.jsx
+++ src/components/pages/TodoPage/TodoPage.jsx
@@ -5,11 +5,14 @@ const TodoPage = ({ getTodoLocal }) => {
 	const { todoID } = useParams();
 	const task1 = getTodoLocal.filter((todo) => todo.id === todoID);
 	console.log(task1);
-
 	return (
 		<div>
-			TODO PAGE
-			<div>{todoID}</div>
+			{task1.map((i) => (
+				<div key={i.id}>
+					<p>{i.title}</p>
+					<p>{i.checked + ''}</p>
+				</div>
+			))}
 		</div>
 	);
 };
